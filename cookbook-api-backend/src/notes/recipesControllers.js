20925a74d28dc7e9ce4bbf4f4e037b405e9424e6
Anna Ostapenko <kvintabudinvest@gmail.com>
2020-02-23 13:24:20 +0200

    removed hadrcored data

--- cookbook-api-backend/src/notes/recipesControllers.js
+++ cookbook-api-backend/src/notes/recipesControllers.js
@@ -4,22 +4,6 @@ require("moment/locale/uk");
 const RecipeModel = require("../models/RecipeDbModel");
 const OldRecipeModel = require("../models/OldRecipeDbModel");
 
-// const recipes = [
-//   {
-//     id: 1,
-//     name: "pizza",
-//     description:
-//       "Preheat the oven before starting to make the dough.Mix the flour, yeast, salt and sugar together in a mixing bowl. Make a well in the centre and add the oil, then add about 225ml water to bring together as a dough. Tip out onto a floured surface and knead for 1 minute, just until smooth. Leave to rest under the upturned bowl while you prepare your pizza toppings.Shape the pizzas (see tip), add your toppings and leave to rest for 10-15 minutes before baking, so that the dough starts to rise.",
-//       isActive: true
-//   },
-//   {
-//     id: 2,
-//     name: "pancake",
-//     description:
-//       "Cut the bananas diagonally into 1.5cm slices. Melt the butter and sugar together in a medium saucepan. Stir until smooth then add the banana slices and cook for 3-4 minutes. Gently stir in the rum, salt and double cream. Serve the boozy bananas and sauce with warm crêpes and a scoop of vanilla ice cream.",
-//   isActive: true
-//     }
-// ];
 
 const getRecipes = (req, res) => {
   RecipeModel.find({
